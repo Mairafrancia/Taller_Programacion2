bc621f2f00a59c46352ba455d500e5488ded078f
Gaby562 <yanezmariagabriela14@gmail.com>
2026-05-13 08:50:09 -0300

    borre duplicado

--- TallerProgramacion2/SegundoRequerimiento/EstadísticasdeSedes.java
+++ TallerProgramacion2/SegundoRequerimiento/EstadísticasdeSedes.java
@@ -21,16 +21,5 @@ public class SegundosRequerimientos{
         System.out.println("Cantidad de partidos en la ciudad " + ciudad + ": " + cantidadPartidos);
     }
 
-    public void estadisticasDeSedesPorCiudad(String ciudad, Mundial mundial) {
-    int cantidadPartidos = 0;
-    for (Sede sede : mundial.getSedes()) {
-        if (sede.getCiudad().equals(ciudad)) {
-            for (Estadio estadio : sede.getEstadios()) {
-                cantidadPartidos += estadio.getPartidos().size();
-            }
-        }
-    }
-    System.out.println("Cantidad de partidos en la ciudad " + ciudad + ": " + cantidadPartidos);
-}
 }
 
